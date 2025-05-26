735dae1359fbb9ce5d25e88be6403be5fc06aaf2
Guilherme <Guilhermev166@outlook.com>
2025-05-26 17:19:50 -0300

    new file:   conexao.php 	new file:   index.php

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'multicultura';
+
+
+$conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+include_once 'conexao.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multicultura</title>
+</head>
+<body>
+    
+</body>
+</html>
